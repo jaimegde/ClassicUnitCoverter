c7f6f77123c1a72a57efeeda14ef1fd909b06b9f
Jaime Garcia de Enterria <jaimenterria@gmail.com>
2024-05-13 10:56:33 +0200

    first commit

---
+++ app/src/main/java/com/example/currencyconverter/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.example.currencyconverter
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.currencyconverter.ui.theme.CurrencyConverterTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CurrencyConverterTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    CurrencyConverter()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun CurrencyConverter(){
+    Column {
+        //stacked below each other
+        Text("Unit Converter")
+        OutlinedTextField(value = "", onValueChange = {})
+        Row {
+            //stacked next to each other
+
+        }
+    }
+}
+
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CurrencyConverterTheme {
+        Greeting("Android")
+    }
+}
